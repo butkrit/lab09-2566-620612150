e45378565afbdc971ad29f64e47784171d0d84a6
butkrit <yokart.butter@gmail.com>
2023-07-20 23:32:55 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,8 @@ export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
 
+  const [taskCompleted, setTaskCompleted] = useState([]);
+  
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
@@ -41,7 +43,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+        All ({tasks.length}) Done ({tasks.filter((task) => task.completed === true).length})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +62,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Thanakrit Yokart" studentId="620612150" />
     </div>
   );
 }
